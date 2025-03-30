b0857d7e30a24e73b8e561ff28d95e13e989b2c0
Noob <90045264+NoobInDaHause@users.noreply.github.com>
2025-03-31 07:26:55 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
+project = 'NoobCogs'
+copyright = '2025, NoobInDaHause'
 author = 'Graziella'
 
 release = '0.1'
-version = '0.1.0'
+version = '1.0.0'
 
 # -- General configuration
 
